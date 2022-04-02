ea08123ead24901ede545cb8577cd93fc4849fd8
Sandra Fernandz <sandraenmunich@gmail.com>
2022-04-02 07:38:41 +0200

    commit

--- recipe/src/App.tsx
+++ recipe/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [recipesFound, setRecipesFound] = useState([]);
+  const [recipeSearch, setRecipeSearch] = useState('');
+
+  const searchForRecipes = async (query: string): Promise<any> => {
+    const result = await fetch(`http://localhost:3001/?search=${query}`);
+    return (await result.json()).results;
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    async () => {
+      const query = encodeURIComponent(recipeSearch);
+      const response = await searchForRecipes(query);
+      setRecipesFound(response);
+    }();
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
